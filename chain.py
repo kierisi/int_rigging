c092898bb4e91f9110067101f26240929a9f80b5
Jesse Mostipak <jesse.maegan@gmail.com>
2024-08-01 18:22:03 -0500

    cleaned up fkchain(), created create_control_from_template()

--- chain.py
+++ chain.py
@@ -9,17 +9,11 @@ def fkchain(parent, joints, template, color):
     for jnt in joints:
         
         # create a control and a group for each joint and rename group and control
-        grp, ctl = cmds.duplicate(template, n=jnt.replace("jnt", "grp"), rc=True)
-        # rc = rename children
-        ctl = cmds.rename(ctl, jnt.replace("jnt", "ctl"))
+        grp, ctl = create_control_from_template(jnt.replace("jnt", "ctl"), joint_parent, template, color)
             
         # position the control on the joint
         #cmds.matchTransform(ctl, jnt) 
         cmds.delete(cmds.parentConstraint(jnt, grp, mo=False))
-            
-        # parent group under the previous control
-        if joint_parent:    # if joint_parent has a value, execute next line
-            cmds.parent(grp, joint_parent)
         
         joint_parent = ctl  # update value of joint_parent to 'ctl' 
             
@@ -28,13 +22,6 @@ def fkchain(parent, joints, template, color):
         cmds.orientConstraint(ctl, jnt)
         cmds.scaleConstraint(ctl, jnt)
 
-        # Freeze transformations
-        #cmds.makeIdentity(ctl, apply=True)
-            
-        # set color
-        cmds.setAttr("{0}.overrideEnabled".format(ctl), 1)
-        cmds.setAttr("{0}.overrideColor".format(ctl), color)
-
 for side in ["lf", "rt"]:
     for phalanx in ["A", "B", "C", "D", "E"]:
         joints = []
@@ -42,3 +29,20 @@ for side in ["lf", "rt"]:
             jnt_name = "{0}_finger{1}0{2}_jnt".format(side, phalanx, i)
             if cmds.objExists(jnt_name):
                 joints.append(jnt_name)
+
+def create_control_from_template(name, parent, template, color):
+    # create a control and a group for each joint and rename group and control
+    grp, ctl = cmds.duplicate(template, n=name.replace("ctl", "grp"), rc=True)
+    # rc = rename children
+    ctl = cmds.rename(ctl, name)
+
+    # parent group under the previous control
+    if parent:    # if joint_parent has a value, execute next line
+        cmds.parent(grp, parent)
+    
+    cmds.setAttr("{0}.overrideEnabled".format(ctl), 1)
+    cmds.setAttr("{0}.overrideColor".format(ctl), color)
+
+    return grp, ctl
+
+def ikchain(name, side, joints, iktemplate, pvtemplate, color):
