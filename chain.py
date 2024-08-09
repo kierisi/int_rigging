85600db3c5f77f0e573830869cc7a292f914c757
Jesse Mostipak <jesse.maegan@gmail.com>
2024-08-08 20:51:54 -0500

    throwing cycling error

--- chain.py
+++ chain.py
@@ -1,7 +1,8 @@
 from maya import cmds
 from maya import OpenMaya as om
 
-def fkchain(parent, joints, template, color):
+def fkchain(parent, joints, template, color, settings_shape):
+    controls = []
     joint_parent = parent
 
     # joints = cmds.ls(sl=True)
@@ -10,18 +11,21 @@ def fkchain(parent, joints, template, color):
     for jnt in joints:
         
         # create a control and a group for each joint and rename group and control
-        grp, ctl = create_control_from_template(jnt.replace("jnt", "ctl"), joint_parent, template, color)
+        grp, ctl = create_control_from_template(jnt.replace("jnt", "ctl"), joint_parent, template, color, settings_shape)
             
         # position the control on the joint
         #cmds.matchTransform(ctl, jnt) 
         cmds.delete(cmds.parentConstraint(jnt, grp, mo=False))
         
         joint_parent = ctl  # update value of joint_parent to 'ctl' 
+        controls.append(ctl)
             
         # drive the joint
         cmds.pointConstraint(ctl, jnt)
         cmds.orientConstraint(ctl, jnt)
         cmds.scaleConstraint(ctl, jnt)
+
+    return controls
 
     for side in ["lf", "rt"]:
         for phalanx in ["A", "B", "C", "D", "E"]:
@@ -166,8 +170,8 @@ def ikchain(name, side, parent, joints, iktemplate, pvtemplate, color, settings_
     jnt1_pinpv_blc = cmds.createNode("blendColors", n="{0}_{1}upperPinPv_blc".format(side, name))
     cmds.connectAttr("{0}.outputX".format(upper_arm_mdn), "{0}.color2R".format(jnt1_pinpv_blc))
     cmds.connectAttr("{0}.distance".format(upper_pv_dbt), "{0}.color1R".format(jnt1_pinpv_blc))
-    cmds.connectAttr("{0}.outputR".format(jnt1_pinpv_blc), "{0}.tx".format(joints[1]))
     cmds.connectAttr("{0}.pinToPv".format(settings_node), "{0}.blender".format(jnt1_pinpv_blc))
+    cmds.connectAttr("{0}.outputR".format(jnt1_pinpv_blc), "{0}.tx".format(joints[1]))
 
     lower_pv_dbt = cmds.createNode("distanceBetween", n="{0}_{1}lowerPinPv_dbt".format(side, name))
     cmds.connectAttr("{0}.worldMatrix[0]".format(ikctl), "{0}.inMatrix1".format(lower_pv_dbt))
@@ -176,5 +180,66 @@ def ikchain(name, side, parent, joints, iktemplate, pvtemplate, color, settings_
     jnt2_pinpv_blc = cmds.createNode("blendColors", n="{0}_{1}lowerPinPv_blc".format(side, name))
     cmds.connectAttr("{0}.outputX".format(lower_arm_mdn), "{0}.color2R".format(jnt2_pinpv_blc))
     cmds.connectAttr("{0}.distance".format(lower_pv_dbt), "{0}.color1R".format(jnt2_pinpv_blc))
-    cmds.connectAttr("{0}.outputR".format(jnt2_pinpv_blc), "{0}.tx".format(joints[2]))
     cmds.connectAttr("{0}.pinToPv".format(settings_node), "{0}.blender".format(jnt2_pinpv_blc))
+    cmds.connectAttr("{0}.outputR".format(jnt2_pinpv_blc), "{0}.tx".format(joints[2]))
+
+    return ikctl, pvctl
+
+# not getting the rig_group created (pick back up at 1:00:05 once sorted)
+def ikfkchain(name, side, parent, joints, fktemplate, iktemplate, pvtemplate, fkcolor, ikcolor):
+    rig_group = cmds.createNode("transform", n="{0}_{1}_grp".format(side, name))
+    if parent:
+        cmds.parent(rig_group, parent)
+    
+    # Settings shape
+    settings_loc = cmds.spaceLocator(n="{0}_{1}settings_loc".format(side, name))
+    cmds.parent(settings_loc, rig_group)
+    settings_shape = cmds.listRelatives(settings_loc, c=True, s=True)[0]
+    cmds.addAttr(settings_shape, at="double", ln="ik", min=0, max=1, dv=0, k=True)
+    
+    # duplicate chains - one for IK, one for FK
+    ik_drivers_grp = cmds.createNode("transform", n="{0}_{1}ikRig_grp".format(side, name))
+    cmds.parent(ik_drivers_grp, rig_group)
+
+    fk_joints = duplicate_chain(joints, rig_group, "_jnt", "fk_jnt")
+    ik_driver_joints = duplicate_chain(joints, ik_drivers_grp, "_jnt", "ikDriver_jnt")
+    ik_joints = duplicate_chain(joints, rig_group, "_jnt", "ik_jnt")
+    blend_joints = duplicate_chain(joints, rig_group, "_jnt", "blend_jnt")
+
+    # connect blend chains
+    for fkjnt, ikjnt, bjnt in zip(fk_joints, ik_joints, blend_joints):
+        for transformation in ["translate", "rotate", "scale"]:
+            blc = cmds.createNode("blendColors", n=bjnt.replace("_jnt", "{0}_jnt".format(transformation.capitalize())))
+            cmds.connectAttr("{0}.{1}".format(ikjnt, transformation), "{0}.color1".format(blc))
+            cmds.connectAttr("{0}.{1}".format(fkjnt, transformation), "{0}.color2".format(blc))
+            cmds.connectAttr("{0}.ik".format(settings_shape), "{0}.blender".format(blc))
+            cmds.connectAttr("{0}.output".format(blc), "{0}.{1}".format(bjnt,transformation))
+
+    for ikdjnt, ikjnt in zip(ik_driver_joints, ik_joints):
+        cmds.pointConstraint(ikdjnt, ikjnt)
+        cmds.orientConstraint(ikdjnt, ikjnt)
+        cmds.scaleConstraint(ikdjnt, ikjnt)
+
+    for bjnt, jnt in zip(blend_joints, joints):
+        cmds.pointConstraint(bjnt, jnt)
+        cmds.orientConstraint(bjnt, jnt)
+        cmds.connectAttr("{0}.scale".format(bjnt), "{0}.scale".format(jnt))
+    
+    # rigging the arm
+    fkcontrols = fkchain(rig_group, fk_joints, fktemplate, fkcolor, settings_shape)
+    ikctl, pvctl = ikchain(name, side, rig_group, ik_driver_joints, iktemplate, pvtemplate, ikcolor, settings_shape)
+
+def duplicate_chain(joints, parent, replace_what, replace_with):
+    new_joints = []
+    new_joints_parent = parent
+
+    for jnt in joints:
+        new_jnt = cmds.duplicate(jnt, n=jnt.replace(replace_what, replace_with), po=True)[0]
+
+        if new_joints_parent:
+            cmds.parent(new_jnt, new_joints_parent)
+        
+        new_joints_parent = new_jnt
+        new_joints.append(new_jnt)
+
+    return new_joints
